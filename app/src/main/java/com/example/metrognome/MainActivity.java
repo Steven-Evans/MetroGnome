a3ea21be79d783dde6dc59108ba1dad00abb8610
Steven Evans <1st3v3n7@gmail.com>
2020-04-14 14:07:31 -0700

    Generate new project with android studio and make my own basic UI

---
+++ app/src/main/java/com/example/metrognome/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.metrognome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void incrementBPM(View view) {
+
+    }
+
+    public void decrementBPM(View view) {
+
+    }
+}
